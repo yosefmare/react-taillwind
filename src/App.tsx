cb83eca8d4558121aff1243bf4869e1c01d82251
Yosef mare <yosefmare108@gmail.com>
2023-11-06 01:03:46 +0200

    add transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import {useState} from 'react'
-import CardComponent from "./components/CardComponent"
-import HeroComponent from "./components/HeroComponent"
-import Buttons from "./components/navbar/Buttons"
-import NavBarComponent from "./components/navbar/NavBarComponent"
-import navContext  from "./context/navbar-context"
-
+import { useState } from 'react';
+import CardComponent from './components/CardComponent';
+import HeroComponent from './components/HeroComponent';
+import Buttons from './components/navbar/Buttons';
+import NavBarComponent from './components/navbar/NavBarComponent';
+import navContext from './context/navbar-context';
+import './App.scss'; // Import your CSS file where the transition effect is defined
 
 const App = () => {
-  const [viability, setViability] = useState<boolean>(false)
-
+  const [viability, setViability] = useState<boolean>(false);
 
   return (
     <div>
-      <navContext.Provider value = {{viability, setViability}}>
-      <NavBarComponent />
-      <div className={viability ? 'absolute lg:hidden sm: block my-3' : 'absolute sm: hidden my-3'}>
-        <Buttons />
-      </div>
-      <HeroComponent />
-      <div className=" my-10 grid lg:grid-cols-3 md:grid-cols-2 sm: grid-cols-1 gap-10 mx-5">
-        <CardComponent />
-      </div>
+      <navContext.Provider value={{ viability, setViability }}>
+        <NavBarComponent />
+        <div className="relative lg:hidden sm:block my-3">
+          <div className={`transition-div ${viability ? 'show' : 'hide'} absolute`}>
+            <Buttons />
+          </div>
+        </div>
+        <HeroComponent />
+        <div className="my-10 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 mx-5">
+          <CardComponent />
+        </div>
       </navContext.Provider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
