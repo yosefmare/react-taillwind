279fd377eca118a72759fe0ff34d5b60ec1513e2
Yosef mare <yosefmare108@gmail.com>
2023-11-11 18:19:59 +0200

    fix sum issues

--- src/components/TestimonialsComponent.tsx
+++ src/components/TestimonialsComponent.tsx
@@ -19,11 +19,12 @@ const TestimonialsComponent = (): JSX.Element => {
         return (
             <div key={index}  className=" bg-slate-200 flex max-h-[440px] overflow-hidden mx-5 mb-10 shadow-lg rounded-md">
                 <div>
-                    <img className="max-w-md rounded-l-md" src={item.img} alt="" />
+                    <img className="max-w-md rounded-l-md" src={item.img} alt={item.alt} />
                 </div>
-                <div>
-                <h2 className=" text-lg">{item.title}</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nemo deserunt incidunt possimus iusto illo at aut. Delectus ea voluptate dolores possimus accusamus cumque corporis, ad laudantium, neque distinctio eveniet.</p>
+                <div className="sm:mx-4 flex flex-col gap-10">
+                <h2 className="text-3xl font-medium mt-5">{item.title}</h2>
+                    <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nemo deserunt incidunt possimus iusto illo at aut. Delectus ea voluptate dolores possimus accusamus cumque corporis, ad laudantium, neque distinctio eveniet.</p>
+                    <button className="btn btn-primary sm:mx-10">more posts</button>
                 </div>
             </div>
         )
@@ -31,12 +32,12 @@ const TestimonialsComponent = (): JSX.Element => {
 
 
     return (
-        <div>
-            <h1 className=" text-4xl text-center my-16">what people say</h1>
+        <>
+            <h1 className=" text-4xl text-center my-16 font-medium">what people say</h1>
             <div className=" grid lg:grid-cols-2 gap-10 sm: grid-cols-1">
                 {content}
             </div>
-        </div>
+        </>
     )
 }
 
